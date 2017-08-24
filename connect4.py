ea24c21f5f4988e5c9a031e5a31fd4d0f2d36cda
Troy <Troy@troysanford.local>
2017-08-24 18:46:00 -0400

    to debug

--- connect4.py
+++ connect4.py
@@ -1,3 +1,24 @@
-def main():
-    print('---')
-main()
+class Game:
+    token = " * "
+    streak = 0
+    victory = False
+    board = [[None for i in range(7)]for j in range(6)]
+
+    def __init__(self):
+        for i in range(6):
+            for j in range(7):
+               self.board[i][j] = " "
+
+    def showBoard(self):
+        a = " "
+        for i in range(5,0):
+            for j in range(7):
+                a += " | " + self.board[i][j]
+        print(a)
+
+class test:
+
+    def main():
+        g = Game()
+        g.showBoard()
+    main()
